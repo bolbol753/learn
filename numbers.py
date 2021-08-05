c1d0e5b2ef3366cdd78033080620589ba76b2d3d
bolbol753 <moh.n.hosny@gmail.com>
2021-08-05 18:29:36 +0200

    "initial commit"

---
+++ numbers.py
@@ -0,0 +1,21 @@
+from math import *
+myNum1 = 5
+myNum2 = -5
+#convert number to string
+str(myNum1)
+#absolute
+print(abs(myNum2))
+#power
+print(pow(myNum1,3))
+#round
+print(round(3.4))
+print(round(3.7))
+#max and min
+print(max(3,4,6,15))
+print(min(2,5,8))
+#approximate to lower integer
+print(floor(3.7))
+#approximate to higher integer
+print(ceil(3.2))
+#square root
+print(sqrt(9))
